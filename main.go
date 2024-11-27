0f59c7ee5bc8754c412e01be4eb1794dedd701da
freddyelgato <freddymoreirala@gmail.com>
2024-11-27 15:20:58 -0500

    feat: :zap: Changes in the files of index.html and main.go
    I show the IPs

--- main.go
+++ main.go
@@ -2,14 +2,61 @@ package main
 
 import (
 	"html/template"
+	"log" // Agregado para manejar logs
+	"net"
 	"net/http"
+	"os" // Agregado para manejar variables de entorno
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("index.html"))
-	tmpl.Execute(w, nil)
+type PageData struct {
+	LocalIP string
 }
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "Unknown"
+	}
+	for _, addr := range addrs {
+		// Verifica si la dirección es una IP y no un localhost
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String()
+			}
+		}
+	}
+	return "Unknown"
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Obtén la IP local
+	ip := getLocalIP()
+
+	// Define la estructura de datos para la plantilla
+	data := PageData{
+		LocalIP: ip,
+	}
+
+	// Carga y ejecuta la plantilla
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("Error ejecutando la plantilla: %v\n", err) // Agregado para manejar errores de ejecución
+	}
+}
+
 func main() {
+	port := os.Getenv("PORT") // Agregado para obtener el puerto desde una variable de entorno
+	if port == "" {
+		port = "8080"                                     // Establece un puerto predeterminado si no está configurado
+		log.Println("Usando puerto predeterminado: 8080") // Agregado para mostrar el puerto utilizado
+	}
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	log.Printf("Servidor iniciado en puerto %s\n", port) // Agregado para loguear el inicio del servidor
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Error iniciando el servidor: %v\n", err) // Agregado para manejar errores al iniciar el servidor
+	}
 }
